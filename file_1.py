ff3f1fea80ddf61dba5f6b3a3ea4cd9e8f0f3460
Rajashekar <aarichary@gmail.com>
2026-05-07 13:20:50 +0530

    c variable added

--- file_1.py
+++ file_1.py
@@ -1,8 +1,9 @@
-def add(a,b):
-    return a+b
+def add(a,b,c):
+    return a+b+c
 
 
 a=int(input("enter your first number"))
 b=int(input('enter your second number'))
+c=int(input("enter third value"))
 
-print(add(a,b))
+print(add(a,b,c))
