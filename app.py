9b341bd8c0132509859a5919428d97e6785fbb30
Rajashekar <aarichary@gmail.com>
2026-05-08 12:10:31 +0530

    added a helper file

--- app.py
+++ app.py
@@ -3,5 +3,15 @@ def greet(name):
     print(f"welcome {name} to the application")
     print("thank you for visiting")
 
-user=input('enter your name')
-greet(user)
+# user=input('enter your name')
+# greet(user)
+
+
+def mul(a,b,c):
+    return a*b*c
+
+n1=int(input("number 1 :"))
+n2=int(input("number 2 :"))
+n3=int(input("number 3 :"))
+
+print(mul(n1,n2,n3))
---
+++ helpers.py
@@ -0,0 +1,25 @@
+def greet(name):
+    print("hello")
+    print(f"welcome {name} to the application")
+    print("thank you for visiting")
+
+
+
+def mul(a,b,):
+    return a*b
+
+def pow(a,b):
+    return a**b
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def div(a,b):
+    return a/b
+
+
+
+
