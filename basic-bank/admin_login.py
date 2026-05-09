426075a97a3546513d99dac53a7cf5e8c64991c5
Rajashekar <aarichary@gmail.com>
2026-05-09 23:10:44 +0530

    added new version of code to adminlogin

--- basic-bank/admin_login.py
+++ basic-bank/admin_login.py
@@ -2,18 +2,14 @@ from databases import *
 import streamlit as st
 
 def control(ad):
-    # admin=input("enter your admin username = ") 
-    admin=st.text_input("enter your admin username = ")
+    admin=st.text_input("enter your admin username = ") 
     login=False
 
     for key,val in ad.items():
         if val['admin_name']==admin:
-            #print("admin is available")
-            st.write("admin is avaialble")
-            #pin=int((input("enter your password = ")))
+            st.write("admin is available")
             pin=st.number_input("enter your password = ")
             if val['pass']==pin:
-                #print("login sucess")
                 st.write("login sucess")
                 login=True
                 break
@@ -21,22 +17,29 @@ def control(ad):
             
     
     if login:
-        #user=input("enter username of a user = ")
         user=st.text_input("enter username of a user = ")
         for key,val in d.items():
             if val['username']==user:
                 val['access']="available"
-                #user_change=input("enter your new name for change = ")
-                user_change=st.text_inut("enter your new name for change = ")
-                if user_change is None:
-                    pass
-                else:
-                    val['username']=user_change
-                #pin_change=input("enter your new pin for change = ")
+                st.write(" access updated")
+        update=st.text_input("Do you want to change details? (yes/no): ")
+        if update.lower()=='yes':
+        
+            st.write(" for username change enter (1), for pin change enter (2) ")
+    
+            change=st.number_input("choose your nummber")
+            if change==1:
+                user_change=st.text_input("enter your new name for change = ")
+                val['username']=user_change
+                st.write(f"your updated username {val['username']}")
+            elif change==2:
                 pin_change=st.text_input("enter your new pin for change = ")
-                if pin_change is None:
-                    pass
-                else:
-                    val['password']=pin_change
+                val['password']=pin_change
+                st.write(f"your updated pin {val['password']}")
+            else:
+                st.write('invalid choice')
+        else:
+            st.write("no changes made")
+            
                     
 
